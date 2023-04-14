1a4ca6a5c2980520095ddca419af7a2199c8da4d
Vugar Badalov <vugarbadalov@Vugars-MacBook-Air.local>
2023-04-14 15:34:19 +0200

    muracietler

--- src/components/Muracietlerim.jsx
+++ src/components/Muracietlerim.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
 function Muracietlerim() {
+    const [data, setData] = useState([
+        { id: 1, name: 'John', surname: 'Doe', phone: '555-1234' },
+        { id: 2, name: 'Jane', surname: 'Doe', phone: '555-5678' },
+        { id: 3, name: 'Bob', surname: 'Smith', phone: '555-9012' },
+      ]);
   return (
-    <div>Muracietlerim</div>
-  )
+    <table className='muracietlerim'>
+      <thead>
+        <tr>
+            <th>Count</th>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>Telephone Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row) => (
+          <tr key={row.id}>
+            <td>{row.id}</td>
+            <td>{row.name}</td>
+            <td>{row.surname}</td>
+            <td>{row.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
+  
 
 export default Muracietlerim
